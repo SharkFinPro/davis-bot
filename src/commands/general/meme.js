24b5a950eec221083c93753ece2a3c43bfb68095
SharkFin <sharkfinpro@hotmail.com>
2019-12-11 17:18:35 -0800

    new random meme api

--- src/commands/general/meme.js
+++ src/commands/general/meme.js
@@ -5,5 +5,5 @@ module.exports = {
     description: 'Shows a random meme',
     type: 'general',
     args: '',
-    command: (message, commandList, config, music) => request('https://some-random-api.ml/meme', (error, response, body) => message.channel.send({files: [JSON.parse(body).image]}))
+    command: (message, commandList, config, music) => request('https://meme-api.herokuapp.com/gimme', (error, response, body) => message.channel.send({files: [JSON.parse(body).url]}))
 };
