7d861a35f86008587bd5cdc94f8826be37745432
SharkFin <sharkfinpro@hotmail.com>
2019-12-11 19:43:17 -0800

    calender update and some other stuff

--- src/commands/general/calendar.js
+++ src/commands/general/calendar.js
@@ -13,14 +13,20 @@ module.exports = {
             .setThumbnail('https://www.troop520redmond.org/system/files/public/graphics/520patch.png')
             .setAuthor(`Troop 520 Calendar`)
         ical.fromURL('https://redmond520.mytroop.us/ical/feed/c2150783-afd0-460a-8991-8392632e75b9', {}, function (err, data) {
-            let today = new Date();
             for (let k in data) {
                 let ev = data[k];
+                let today = new Date();
                 let startDate = new Date(ev.start);
+                let endDate = new Date(ev.end);
                 if (startDate.getFullYear() === today.getFullYear()) {
                     if (startDate.getMonth() + 1 === today.getMonth() + 1) {
-                        if (ev.location !== '') calendar.addField(`${startDate.getMonth() + 1}/${startDate.getDate()}/${startDate.getFullYear()} @${ev.location}`, `${ev.summary}`);
-                        else calendar.addField(`${startDate.getMonth() + 1}/${startDate.getDate()}/${startDate.getFullYear()}`, `${ev.summary}`);
+                        if (ev.location !== '') {
+                            if (startDate.getDate() === endDate.getDate()) calendar.addField(`${startDate.getMonth() + 1}/${startDate.getDate()}/${startDate.getFullYear()} @${ev.location} ${startDate.toLocaleTimeString().replace(/:\d+ /, ' ')} > ${endDate.toLocaleTimeString().replace(/:\d+ /, ' ')}`, `${ev.summary}`);
+                            else calendar.addField(`${startDate.getMonth() + 1}/${startDate.getDate()}/${startDate.getFullYear()} @${ev.location}`, `${ev.summary}`);
+                        } else {
+                            if (startDate.getDate() === endDate.getDate()) calendar.addField(`${startDate.getMonth() + 1}/${startDate.getDate()}/${startDate.getFullYear()} ${startDate.toLocaleTimeString().replace(/:\d+ /, ' ')} > ${endDate.toLocaleTimeString().replace(/:\d+ /, ' ')}`, `${ev.summary}`);
+                            else calendar.addField(`${startDate.getMonth() + 1}/${startDate.getDate()}/${startDate.getFullYear()}`, `${ev.summary}`);
+                        }
                     }
                 }
             }
