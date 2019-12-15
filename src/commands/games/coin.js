dfcf590133f65dcc2ca64fd8eb3a630e32315ebe
SharkFin <sharkfinpro@hotmail.com>
2019-12-15 15:57:02 -0800

    Added new commands
    Added coinflip and dice rolling commands

---
+++ src/commands/games/coin.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    enabled: true,
+    description: 'Flip a coin',
+    type: 'games',
+    args: '',
+    command: (message, bot) => {
+        message.channel.send(Math.floor((Math.random() * 2)) === 0 ? "Heads!" : "Tails!");
+    }
+};
---
+++ src/commands/games/dice.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    enabled: true,
+    description: 'Roll a die',
+    type: 'games',
+    args: ['sides'],
+    command: (message, bot) => {
+        let diceSides = Math.floor(message.content.toLowerCase().split(' ')[1]) || 6;
+        message.channel.send(`The die rolled a ${Math.floor((Math.random() * diceSides) + 1)}!`);
+    }
+};
