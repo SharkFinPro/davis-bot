f1e3882adf9832a0f8273743b9e1747fbf4083cb
SharkFin <sharkfinpro@hotmail.com>
2020-07-04 16:46:05 -0700

    patrol room toggle
     - !tpr command added to toggle patrol room visibility

---
+++ src/commands/general/tpr.js
@@ -0,0 +1,87 @@
+const patrols = [ // Role, Text Channel, Voice Channel
+  ["Falcons", "falcons", "Falcons"],
+  ["Wombats", "wombats", "Wombats"],
+  ["Buffaloes", "buffaloes", "Buffaloes"],
+  ["Beast Lizards", "beast-lizards", "Beast Lizards"],
+  ["Scorpions", "scorpions", "Scorpions"],
+  ["Squirrels", "squirrels", "Squirrels"]
+];
+module.exports = {
+  enabled: true,
+  description: "Unlock/Lock Patrol Rooms",
+  args: "",
+  type: "general",
+  command(message, bot) {
+    let toggle = false;
+    for (let p of patrols) {
+      let patrol = message.guild.roles.cache.find((role) => role.name === p[0]);
+      toggle = this.togglePerms(message, message.guild.channels.cache.find((channel) => channel.name === p[1]), patrol);
+      this.togglePerms(message, message.guild.channels.cache.find((channel) => channel.name === p[2]), patrol);
+    }
+    message.channel.send(toggle ? "Opened Patrol Rooms" : "Closed Patrol Rooms");
+  },
+  togglePerms(message, channel, patrol) {
+    let spl = message.guild.roles.cache.find((role) => role.name === "SPL");
+    let aspl = message.guild.roles.cache.find((role) => role.name === "ASPL");
+    let adult = message.guild.roles.cache.find((role) => role.name === "Adult");
+    let member = message.guild.roles.cache.find((role) => role.name === "member");
+
+    if (channel.permissionsFor(patrol).has("VIEW_CHANNEL")) {
+      channel.overwritePermissions([
+        {
+           id: spl,
+           deny: ["VIEW_CHANNEL"]
+        },
+        {
+           id: aspl,
+           deny: ["VIEW_CHANNEL"]
+        },
+        {
+           id: adult,
+           deny: ["VIEW_CHANNEL"]
+        },
+        {
+           id: member,
+           deny: ["VIEW_CHANNEL"]
+        },
+        {
+           id: patrol,
+           deny: ["VIEW_CHANNEL"]
+        },
+        {
+           id: message.guild.roles.everyone.id,
+           deny: ["VIEW_CHANNEL"]
+        }
+      ], "Closed Patrol Rooms");
+      return false;
+    }
+    channel.overwritePermissions([
+      {
+         id: spl,
+         allow: ["VIEW_CHANNEL", "SEND_MESSAGES", "CONNECT"]
+      },
+      {
+         id: aspl,
+         allow: ["VIEW_CHANNEL", "SEND_MESSAGES", "CONNECT"]
+      },
+      {
+         id: adult,
+         allow: ["VIEW_CHANNEL", "SEND_MESSAGES", "CONNECT"]
+      },
+      {
+         id: member,
+         allow: ["VIEW_CHANNEL"],
+         deny: ["SEND_MESSAGES", "CONNECT"]
+      },
+      {
+         id: patrol,
+         allow: ["VIEW_CHANNEL", "SEND_MESSAGES", "CONNECT"]
+      },
+      {
+         id: message.guild.roles.everyone.id,
+         deny: ["VIEW_CHANNEL"]
+      }
+    ], "Opened Patrol Rooms");
+    return true;
+  }
+};
