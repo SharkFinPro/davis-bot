675d8cdfcff879295574f2f4b9745f0616b0d8f2
SharkFin <sharkfinpro@hotmail.com>
2020-07-05 17:43:54 -0700

    fix commands
     - fixed serverinfo command
     - fixed nextmeeting & calendar commands by going to an old version of ical (update later possibly)

--- src/commands/general/serverinfo.js
+++ src/commands/general/serverinfo.js
@@ -11,16 +11,16 @@ module.exports = {
       .setThumbnail(message.guild.iconURL())
       .setAuthor(`${message.guild.name} | Server Info`, message.guild.iconURL())
       .addField("Name", message.guild.name, true)
-      .addField("Roles", message.guild.roles.size, true)
+      .addField("Roles", message.guild.roles.cache.size, true)
       .addField("Members", message.guild.memberCount, true)
       .addField("ID", message.guild.id, true)
       .addField("Owner", message.guild.owner.user.tag, true)
-      .addField("Humans", message.guild.members.filter((member) => !member.user.bot).size, true)
+      .addField("Humans", message.guild.members.cache.filter((member) => !member.user.bot).size, true)
       .addField("Region", message.guild.region, true)
       .addField("Owner ID", message.guild.ownerID, true)
-      .addField("Bots", message.guild.members.filter((member) => member.user.bot).size, true)
-      .addField("Channels", message.guild.channels.size, true)
-      .addField("Online", message.guild.members.filter((member) => member.user.presence.status !== "offline").size, true)
-      .addField("Custom Emojis", message.guild.emojis.size, true));
+      .addField("Bots", message.guild.members.cache.filter((member) => member.user.bot).size, true)
+      .addField("Channels", message.guild.channels.cache.size, true)
+      .addField("Online", message.guild.members.cache.filter((member) => member.user.presence.status !== "offline").size, true)
+      .addField("Custom Emojis", message.guild.emojis.cache.size, true));
   }
 };
